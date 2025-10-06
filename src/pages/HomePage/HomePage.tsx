b32434b7545dbb2b0f53ff5de9be8d9fe8e30b1a
melorenzz <gomisha552@gmail.com>
2025-10-06 11:57:05 +0200

    Added quantity changer in the cart

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import MainLayout from "../../layouts/MainLayout.tsx";
-import Search from "../../components/Home/Search.tsx";
 import PopularRestaurants from "../../components/Home/PopularRestaurants.tsx";
 import WhatOrderIn from "../../components/Home/WhatOrderIn.tsx";
 import Banner from "../../components/Home/Banner.tsx";
